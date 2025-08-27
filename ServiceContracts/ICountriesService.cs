5bc600c7c390ea74a1ed3c7017a2ee72044ebc8e
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-08-26 21:34:45 -0400

    Add project files.

---
+++ ServiceContracts/ICountriesService.cs
@@ -0,0 +1,17 @@
+﻿using ServiceContracts.DTO;
+
+namespace ServiceContracts
+{
+    /// <summary>
+    /// Represents business logic fro manipulating Country entity
+    /// </summary>
+    public interface ICountriesService
+    {
+        /// <summary>
+        /// Adds a country to the list of countries
+        /// </summary>
+        /// <param name="countryAddRequest">Country object to add</param>
+        /// <returns>Returns the country object after adding it (including newly generated id)</returns>
+        CountryResponse AddCountry(CountryAddRequest? countryAddRequest);
+    }
+}
